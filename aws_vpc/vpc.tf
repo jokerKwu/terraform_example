adc9f024c6aebff0270c3add9e736cf9678a3eba
RyanBreathings <ryan@breathings.co.kr>
2022-12-27 19:54:59 +0900

    인터넷 게이트웨이 생성, 라우트 테이블 설정, private subnet 생성

--- aws_vpc/vpc.tf
+++ aws_vpc/vpc.tf
@@ -32,3 +32,51 @@ resource "aws_subnet" "second_subnet" {
     Name = "101subnet-2"
   }
 }
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main"
+  }
+}
+
+resource "aws_route_table" "route_table" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main"
+  }
+}
+
+resource "aws_route_table_association" "route_table_association_1" {
+  subnet_id      = aws_subnet.first_subnet.id
+  route_table_id = aws_route_table.route_table.id
+}
+
+resource "aws_route_table_association" "route_table_association_2" {
+  subnet_id      = aws_subnet.second_subnet.id
+  route_table_id = aws_route_table.route_table.id
+}
+
+resource "aws_subnet" "first_private_subnet" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.3.0/24"
+
+  availability_zone = "us-west-2a"
+
+  tags = {
+    Name = "101subnet-private-1"
+  }
+}
+
+resource "aws_subnet" "second_private_subnet" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.4.0/24"
+
+  availability_zone = "us-west-2b"
+
+  tags = {
+    Name = "101subnet-private-2"
+  }
+}
